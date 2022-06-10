5f610246192fa6583f06faaebb811f5cf761f057
osamataqieden <otaqiedeen@gmail.com>
2022-06-10 22:39:55 +0300

    ff

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -42,6 +42,12 @@ namespace CloudDemo.Controllers
             {
                 HttpContext.Session.SetString("ID", responseModel.externalId);
                 HttpContext.Session.SetString("Role", responseModel.role);
+                HttpContext.Session.SetString("UserName", responseModel.username);
+                //Check isNew
+                if (IsNewUser(loginModel))
+                {
+                    return View("ResetPassword");
+                }
                 if (responseModel.role == "Student")
                 {
                     return RedirectToAction("Index", "Students");
@@ -56,6 +62,35 @@ namespace CloudDemo.Controllers
                 ViewBag.InvalidUserNameOrPassword = "Y";
                 return View();
             }
+        }
+
+        [HttpPost]
+        public IActionResult ResetPassword(string NewPassword)
+        {
+            var UserName = HttpContext.Session.GetString("UserName");
+            var Password = "1234";
+            HttpClient client = new HttpClient();
+            var entity = JsonSerializer.Serialize(new
+            {
+                username = UserName,
+                password = Password,
+                newPassword = NewPassword
+            });
+            var requestContent = new StringContent(entity, Encoding.UTF8, "application/json");
+            var result = client.PostAsync("http://158.101.231.162:8080/api/rest/iam/login/changePassword", requestContent).Result;
+            if(result.IsSuccessStatusCode)
+                return RedirectToAction("Index");
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+        private bool IsNewUser(LoginModel model)
+        {
+            HttpClient client = new HttpClient();
+            var response = client.GetStringAsync($"http://158.101.231.162:8080/api/rest/iam/login/isNewUser/email/{model.username}").Result;
+            if (string.IsNullOrEmpty(response) || response == "false")
+                return false;
+            return true;
         }
     }
 }
